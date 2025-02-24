4a5a92e8748049e804d659eab7ea9866274a35b6
phon dara <phondara3333@gmail.com>
2025-02-24 18:51:43 +0700

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,10 @@
+
+// JavaScript to show only the first 2 images
+// document.addEventListener("DOMContentLoaded", function() {
+//     var images = document.querySelectorAll('.container-gallery .img-gallery img');
+//     for (var i = 0; i < images.length; i++) {
+//         if (i < 2) {
+//             images[i].style.display = 'none '; // Show the first 2 images
+//         }
+//     }
+// });
